bc67626aae772b565a66f5c5d892adbcfc5fbe5c
thomas444ortiz <thomasortiz444@gmail.com>
2024-02-20 15:13:00 -0500

    finish initial setup of codebase

---
+++ client/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App():React.JSX.Element {
+  return (
+    <div>Hello World</ div>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
     entry: './client/Index.tsx', // tells webpack which file to start building its dependency tree from
@@ -6,20 +7,19 @@ const config = {
         path: path.resolve(__dirname, 'dist'), // tells webpack to output the bundle into a folder called dist, in the current directory
             filename: 'bundle.js' // call our output file bundle.js
     },
-    devServer: { // configure out dev server
-        static: {
-            directory: path.join(__dirname, './static'), 
-        },
-        proxy: { // set up a proxy so that any request made to our dev server, that starts with /api, gets rerouted to 3000
-            '/api': {
+    devServer: {
+        proxy: [
+            {
+              context: ['/'],
               target: 'http://localhost:3000',
-              pathRewrite: { '^/api': '' }, // rewrite our requests to remove /api from the route
-            }
-        },
-        hot: true,
-        compress: true,
-        port: 8080 // tells us to run devserer on 8080
+            },
+        ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './client/static/index.html',
+        }),
+      ],
     module: {
         rules: [ // define a bunch of rules for transpiling our code
             {
