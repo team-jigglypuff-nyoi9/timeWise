b5c69b35f86f786cc188606c461e8672b616894f
Wu-Cheney <maxiwu123@gmail.com>
2024-02-20 18:52:08 -0500

    Created post route for schedules to add to db
    Co-authored-by: Jesse <jguerrero35@users.noreply.github.com>

---
+++ server/controllers/scheduleController.ts
@@ -0,0 +1,33 @@
+import { NextFunction } from "express";
+import { RequestBody } from "../types";
+
+
+const db = require('../models/models.js');
+
+
+const scheduleController = {
+    addSchedule: async (req: Request, res: Response, next: NextFunction) => {
+
+        try {
+            if (req.body && typeof req.body === 'object' && 'schedText' in req.body && 'userId' in req.body) {
+                const reqBody = req.body as RequestBody;
+                const queryObj = {
+                    text: `INSERT into Schedules (sched_text, user_id) VALUES ($1, $2)`,
+                    values: [reqBody.schedText, reqBody.userId]
+                };
+                const results = db.query(queryObj);
+            }
+           
+        } catch (error) {
+            const errObj = {
+                log: 'An unknown error occurred while trying to add a schedule',
+                status: 500,
+                message: 'An error occurred'
+            }
+            return next(errObj);
+        }
+    }
+};
+
+
+module.exports = scheduleController;
